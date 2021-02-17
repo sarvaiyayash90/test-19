af007f51e26d071d3d7e53a82ad931d8c158187b
sarvaiyayash90 <sarvaiyayash90@gmail.com>
2021-02-18 00:18:33 +0530

    sas

--- server.js
+++ server.js
@@ -282,9 +282,9 @@ app.get('/viewloginprofile/:id', async (req, res) => {
 // Login Controller
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('./client/build'))
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'..','client','build','index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
