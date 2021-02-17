6f304890a102cb6da000b31830d4b19ee67ed8a6
sarvaiyayash90 <sarvaiyayash90@gmail.com>
2021-02-17 16:49:07 +0530

    sas

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ var login_controller = require('./Controllers/Login_Controller')
 
 var app = express();
 
-app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");//Authorization, sid
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Credentials', "true");
-  next();
-});
+// app.use(function(req, res, next){
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");//Authorization, sid
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.header('Access-Control-Allow-Credentials', "true");
+//   next();
+// });
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
