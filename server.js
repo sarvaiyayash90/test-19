97a24ce4b2ca6d7377ba4b681e1b7188b49d8337
sarvaiyayash90 <sarvaiyayash90@gmail.com>
2021-02-17 18:11:49 +0530

    sas

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path');
 require("dotenv").config();
 
 const student = require('./Models/Student_Model'); // Model import
+const login_schema = require('./Models/Login_Model'); // Login Model
 
 const multer = require('multer'); // Multer one type image upload time call middleware
 
@@ -19,7 +20,7 @@ var PDFDocument = require('pdfkit'); // PDF
 
 
 //var student_controller = require('./Controllers/Student_Controller')
-var login_controller = require('./Controllers/Login_Controller')
+// var login_controller = require('./Controllers/Login_Controller')
 
 var app = express();
 
@@ -225,10 +226,77 @@ app.put('/UpdateStudent/:id',(req, res) => {
         }
     })
 })
-  
+
+// login
+
+/*
++--------------------------+
+|      Login Check         |
++--------------------------+  */
+
+app.post('/Login',(request, res) => {
+
+  console.log("Body Data", request.body)
+
+  var email = request.body.email_id;
+  var password = request.body.password;
+
+  if (email && password)
+  {
+      login_schema.find({ email_id: email, password: password }, function (err, user) {
+      if (user.length > 0)
+      {
+        console.log("Login Successfully");
+        //request.session.loggedin = true;
+        // request.session.username = request.body.email_id;
+        // console.log("s",request.session.username);
+        res.status(200).send({
+          user:user,
+          auth:true,
+          session:user[0]._id,
+          message:"Login successfully"
+        })
+      }
+      else
+      {
+        res.status(404).send({
+          auth:false,
+          message:"Incorrect Username and/or Password!"
+        })
+        console.log("Incorrect Username and/or Password!");
+      }
+      res.end()
+    });
+    //request.session.username = request.body.email_id;
+    //console.log("go",request.session.username)
+  }
+  else
+  {
+    res.status(500).send({message:"Please enter Username and Password!"})
+    res.send('Please enter Username and Password!');
+    res.end();
+  }
+
+})
+
+/*  +--------------------------+
+    |     Single data show     |
+    +--------------------------+  */
+    
+app.get('/viewloginprofile/:id',async (req, res) => {
+  try {
+      const login_schema_new = await login_schema.findById(req.params.id)
+      //console.log(login_schema_new);
+      res.json(login_schema_new)
+  } catch (err) {
+      res.send('Error' + err)
+  }
+})
+    
 // app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 
 //app.use('/studentdata',student_controller); // Student controller
+//app.use('/logindata',login_controller); 
 
 // Login Controller
 
@@ -239,7 +307,7 @@ if(process.env.NODE_ENV === "production"){
   });
 }
 
-app.use('/logindata',login_controller); 
+
 
 app.listen(process.env.PORT || 3100,()=>{console.log("\n \t\t\t < Server Started At Port : (3100) > \n ")});
 
