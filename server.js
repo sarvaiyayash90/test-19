d1a5f94010546cfb2bd1fe39bcd16119f36afac9
sarvaiyayash90 <sarvaiyayash90@gmail.com>
2021-02-17 17:25:20 +0530

    sas

--- server.js
+++ server.js
@@ -23,19 +23,18 @@ var login_controller = require('./Controllers/Login_Controller')
 
 var app = express();
 
-// app.use(function(req, res, next){
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");//Authorization, sid
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.header('Access-Control-Allow-Credentials', "true");
-//   next();
-// });
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");//Authorization, sid
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Credentials', "true");
+  next();
+});
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json());
 //app.use(cors({origin:'http://localhost:3000'}))
-app.use(cors({origin:'https://yash-19.herokuapp.com'}))
-
+app.use(cors())
 
 // const storage = multer.diskStorage({
 //   destination: function (req, file, cb) {
