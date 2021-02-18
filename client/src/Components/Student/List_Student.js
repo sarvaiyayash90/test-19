7537dd0db70706e1648172322d6a314b93c95c83
sarvaiyayash90 <sarvaiyayash90@gmail.com>
2021-02-18 15:11:23 +0530

    sas

--- client/src/Components/Student/List_Student.js
+++ client/src/Components/Student/List_Student.js
@@ -16,7 +16,6 @@ const List_Student = () => {
     const load_student_data = async () => {
         const result = await axios.post(`https://yash-19.herokuapp.com/liststudent/${localStorage.getItem('Token_Key')}`);
         setStudent(result.data.reverse());
-        window.location.href=`/liststudent/${localStorage.getItem('Token_Key')}`
     }
 
     const delete_student = async id => {
@@ -72,9 +71,9 @@ const List_Student = () => {
                     <div style={{ margin: '0 0 0 50px' }}>
                         <h1 style={{ fontSize: '40px', fontFamily: 'cursive' }}>Students List Page</h1>
                     </div>
-                    {/* <div style={{ margin: '0 0 10px 985px' }}>
+                    <div style={{ margin: '0 0 10px 985px' }}>
                         <Link style={{border:'none'}} className="btn btn-outline-warning btn-lg" onClick={() => { if (window.confirm('Are you sure you wish to create CSV ?')) csv(localStorage.getItem('Token_Key')); fetch_CSV(localStorage.getItem('Token_Key')) }} ><i class="far fa-file-csv fa-2x"></i></Link>
-                    </div> */}
+                    </div>
                 </div>
 
                 <table className="table border">
